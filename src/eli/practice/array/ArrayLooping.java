ccd2af1bf9fbb1eb6cb3ac06551e1587283dfd59
Ryan Pelletier <ryanmatthewpelletier@gmail.com>
2020-03-08 14:42:39 -0400

    add ArrayLooping practice

---
+++ src/eli/practice/array/ArrayLooping.java
@@ -0,0 +1,47 @@
+package eli.practice.array;
+
+public class ArrayLooping {
+
+    public static void main(String[] args){
+
+        int[] arrayOfIntegers = {1, 32, 23, 4, 15, 6, 17, 8, 9, 10, 3, 2, 1};
+
+        printArray(arrayOfIntegers);
+
+        System.out.println("The smallest number is " + getSmallestNumber(arrayOfIntegers));
+        System.out.println("The largest number is " + getLargestNumber(arrayOfIntegers));
+        System.out.println("The average number is " + getAverageValue(arrayOfIntegers));
+
+    }
+
+
+
+
+    /*
+        TODO: Eli, write the actual code in the following 4 methods.
+     */
+    public static void printArray(int[] array){
+
+    }
+
+
+    public static int getSmallestNumber(int[] array){
+
+
+        return -1;
+    }
+
+
+    public static int getLargestNumber(int[] array){
+
+        return -1;
+    }
+
+
+    public static double getAverageValue(int[] array){
+
+        return -1.0;
+    }
+
+
+}
